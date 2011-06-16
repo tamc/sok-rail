c88d6849f3eacf190ba7bdca098b414038df1212
tamc <tamc@greenonblack.com>
2011-06-16 20:20:45 +0100

    Empty rails 3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SokRailMongo::Application.config.secret_token = '83c602a85c4db99f0e1da0af06b4fc7403c6d757866125cf5fd4adaf6eec29994636807f77129c91f1699cb0301d043e3ec177718673d458e4e5ca0069f8aeb9'
